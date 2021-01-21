947d830574c080e06c7b3751a371d7cf915381bd
itsnou <emiliano.meneses@gmail.com>
2021-01-21 13:23:37 -0300

    nuevo menu, nuevo banner

--- js/sweetAlert.js
+++
@@ -1,4 +0,0 @@
-Swal.fire({
-    title: 'Bienvenido a Dietetica San Cayetano',
-    html: 'Recorda que con compras superiores a $5000(pesos argentinos) los envios son <b class="color__alert">gratis!</b>',
-})
